bd5696411b9f009827cad6896eaa5feb9de8ba89
NancyVeSo <nancyestefaniavs@gmail.com>
2019-08-02 16:25:41 -0500

    Creación de migraciones, modelos y controladores

---
+++ app/Equipo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Equipo extends Model
+{
+    protected $table = "equiposses";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+}
---
+++ app/Jugador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jugador extends Model
+{
+    protected $table = "jugadoresses";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+}
